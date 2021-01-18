c6e15f20b50b97ee7374ea91119850ca9bb5418d
Sheema Parwaz <Sheemaparwaz@gmail.com>
2021-01-18 20:05:54 +0530

    basic debugging done

--- sketch.js
+++ sketch.js
@@ -10,27 +10,26 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+    createCanvas(800, 650);
+    
+    paperSprite=createSprite(width/2, 80, 10,10);
+	paperSprite.scale=0.2
 
+	dustbinSprite=createSprite(width/2, 200, 10,10);
+	dustbinSprite.scale=0.6
 
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-
-
-	Engine.run(engine);
-  
+	groundSprite=createSprite(width/2, height-35, width,10);
+    groundSprite.shapeColor=color(255)
 }
-
-
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+        rectMode(CENTER);
+        background(0);
+		drawSprites();
 }
 
+function keyPressed() {
+            if (keyCode === UP_ARROW) {
 
-
+                Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:85});
+            }
+}
